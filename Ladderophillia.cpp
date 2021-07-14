086098178f53be7f2721fb4c4b15b2758169a7bf
Rakshith Sudan <55922520+Rakshith-Sudan@users.noreply.github.com>
2021-07-14 11:22:44 +0530

    Create Ladderophillia.cpp

---
+++ Ladderophillia.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+
+	while(n!=0)
+	{
+		cout<<"*   *"<<endl;
+		cout<<"*   *"<<endl;
+		cout<<"*****"<<endl;;
+		n--;
+	}
+	cout<<"*   *"<<endl;
+	cout<<"*   *"<<endl;
+}
